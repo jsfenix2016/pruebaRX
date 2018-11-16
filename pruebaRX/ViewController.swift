d1ad1636db12af3e5597f36c07e6ba4d1f8193fc
javier <j.santtana.franklin@accenture.com>
2018-11-17 00:19:00 +0100

    Initial Commit

---
+++ pruebaRX/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  pruebaRX
+//
+//  Created by test on 17/11/18.
+//  Copyright © 2018 jsfenix. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
